3778a04fb51708dae8e2a27f2aa0a805d47a4840
Jack Kenney <kenney.jnk@gmail.com>
2017-11-04 20:14:03 -0400

    rest framework fix

--- umassaccess/umassaccess/settings.py
+++ umassaccess/umassaccess/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'umapp.apps.UmappConfig'
+    'umapp.apps.UmappConfig',
     'rest_framework'
 ]
 
